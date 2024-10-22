87d2e6afc2420a1f1afb577fb716afa4af10b7ae
Pavi302005 <Pavip9867@gmail.com>
2024-10-22 22:14:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function solve()
+{
+    let v=document.querySelector('.input1').value;
+    let x=eval(v);
+    document.querySelector('.input1').value=x;
+    return x;
+}
+function close()
+{
+    let r=documnet.querySelector('.input1').value="0";
+    return r;
+}
+function name()
+{
+    let del=document.querySelector('.input1');
+    del.value=" ";
+}
